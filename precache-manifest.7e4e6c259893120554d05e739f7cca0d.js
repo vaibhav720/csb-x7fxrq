0f0b1badf4b947f16632b3235accd3e0c6dde6a5
Code Sandbox <hello@codesandbox.io>
2022-04-08 12:34:13 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7e4e6c259893120554d05e739f7cca0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f17e09f043909481cdf1466fc2147a9",
+    "url": "https://vaibhav720.github.io/csb-x7fxrq/index.html"
+  },
+  {
+    "revision": "b9302d8871f2db8d116a",
+    "url": "https://vaibhav720.github.io/csb-x7fxrq/static/css/main.c15a2f71.chunk.css"
+  },
+  {
+    "revision": "311b856d4cbf83c3e616",
+    "url": "https://vaibhav720.github.io/csb-x7fxrq/static/js/2.f362281c.chunk.js"
+  },
+  {
+    "revision": "b9302d8871f2db8d116a",
+    "url": "https://vaibhav720.github.io/csb-x7fxrq/static/js/main.9640a4e7.chunk.js"
+  },
+  {
+    "revision": "becc3d555e5e6d9fa37b",
+    "url": "https://vaibhav720.github.io/csb-x7fxrq/static/js/runtime~main.1623d421.js"
+  }
+]);
